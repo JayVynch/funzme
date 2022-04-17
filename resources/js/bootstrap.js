4912c9424f584915c78252fe4e289bc97b850a63
Jacob George <kooldrjay060@gmail.com>
2022-04-17 13:40:09 +0100

    setting websockets for front end

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,8 +24,9 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
-    wsPort: 6001,
-    wssPort: 6001,
-    forceTLS: false,
+    wsPort: window.APP_DEBUG ? 6001 : 6002,
+    wssPort: window.APP_DEBUG ? 6001 : 6002,
+    forceTLS: !window.APP_DEBUG,
     disableStats: true,
+    enabledTransports: ['ws', 'wss'],
 });
